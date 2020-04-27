680ed76da348aefa8274500a6a8e5d34cb086fd8
shsangha <sssangha@ucalgary.ca>
2020-04-27 14:29:02 -0600

    module declarations for scss modules and gsap

---
+++ src/typings.d.ts
@@ -0,0 +1,9 @@
+declare module "*.scss"
+
+declare module "*DrawSVGPlugin"
+
+declare module "body-scroll-lock"
+
+declare module "*EasePack.min.js"
+
+declare module "gatsby-plugin-transition-link"
